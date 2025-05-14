725c6d124e8e070fce871e8afc2a9d1a1b625812
[Bert Christiaens] <gretzkyohara@gmail.com>
2025-05-14 15:04:26 +0200

    added repository

--- src/controller/OrderSystem.java
+++ src/controller/OrderSystem.java
@@ -9,6 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/*
+Class to handle incoming Orders from user interface
+
+ */
 public class OrderSystem {
 
     OrderRepository repo = new FileOrderRepository();
@@ -16,22 +20,35 @@ public class OrderSystem {
     public OrderSystem() {
     }
 
+    /*
+    method to add Order to the repo
+     */
     public void addOrder (Order order) {
         repo.addOrder(order);
 
     }
 
+    /*
+    method to getOrders by Person
+
+     */
     public List<Order> getOrdersByPerson(String aPersonName){
 
       return repo.getOrdersByPerson(aPersonName);
 
     }
 
+    /*
+    method to remove order from repo
+     */
     public void removeOrder(String aPersonName, String aSandwichName) throws IOException {
         repo.removeOrder(aPersonName, aSandwichName);
 
     }
 
+    /*
+    method to print orders
+     */
     public void printOrder() throws IOException {
 
         repo.printOrder();
